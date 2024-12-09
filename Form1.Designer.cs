b72545c74358872b4cd65b856b10677b75bd9d19
JaThinh <phamlegiathinh2004@gmail.com>
2024-12-09 12:54:05 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace BTVN_Buoi03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.txtSTT = new System.Windows.Forms.TextBox();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(171, 49);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(383, 318);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // txtSTT
+            // 
+            this.txtSTT.Location = new System.Drawing.Point(171, 21);
+            this.txtSTT.Multiline = true;
+            this.txtSTT.Name = "txtSTT";
+            this.txtSTT.Size = new System.Drawing.Size(118, 22);
+            this.txtSTT.TabIndex = 0;
+            this.txtSTT.TextChanged += new System.EventHandler(this.txtSTT_TextChanged);
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(306, 21);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(129, 22);
+            this.txtMa.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(441, 21);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(113, 22);
+            this.txtName.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(591, 74);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(115, 27);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(591, 117);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(115, 23);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(591, 157);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(115, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtMa);
+            this.Controls.Add(this.txtSTT);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox txtSTT;
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTVN_Buoi03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetupListView();
+        }
+        private void SetupListView()
+        {
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+
+            // Thêm các cột
+            listView1.Columns.Add("STT", 100);
+            listView1.Columns.Add("Mã", 100);
+            listView1.Columns.Add("Tên", 100);
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtSTT.Text != "" && txtMa.Text != "" && txtName.Text != "")
+            {
+                ListViewItem item = new ListViewItem(txtSTT.Text);
+                item.SubItems.Add(txtMa.Text);
+                item.SubItems.Add(txtName.Text);
+                listView1.Items.Add(item);
+
+                // Xóa text sau khi thêm
+                txtSTT.Clear();
+                txtMa.Clear();
+                txtName.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ dữ liệu!");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                listView1.Items.Remove(listView1.SelectedItems[0]);
+            }
+            else
+            {
+                MessageBox.Show("Hãy chọn một dòng để xóa!");
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                selectedItem.SubItems[0].Text = txtSTT.Text;
+                selectedItem.SubItems[1].Text = txtMa.Text;
+                selectedItem.SubItems[2].Text = txtName.Text;
+
+                // Xóa text sau khi sửa
+                txtSTT.Clear();
+                txtMa.Clear();
+                txtName.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Hãy chọn một dòng để sửa!");
+            }
+        }
+
+        private void txtSTT_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
